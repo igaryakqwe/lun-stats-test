f23b9d84cb358bf6b554ab2e2eaf0b8313a33e42
Igor <114025058+igaryakqwe@users.noreply.github.com>
2024-04-25 11:54:39 +0300

    changed api

--- src/app/api/reimbursement/route.ts
+++ src/app/api/reimbursement/route.ts
@@ -6,10 +6,8 @@ import { convertReimbursements } from "@/utils/convertReimbursements";
 export async function GET(request: NextRequest): Promise<Response> {
     const searchParams = request.nextUrl.searchParams;
     const year = Number(searchParams.get("year")) || 2022;
-    const startMonth = Number(searchParams.get("startMonth")) || 1;
-    const endMonth = Number(searchParams.get("endMonth")) || 12;
     const reimbursementsRaw = fs.readFileSync(`./src/data/${year}.json`, "utf8");
     const reimbursements: AllPayment[] = JSON.parse(reimbursementsRaw);
-    const response = convertReimbursements(reimbursements, year, startMonth, endMonth);
+    const response = convertReimbursements(reimbursements, year);
     return NextResponse.json(response);
 }
